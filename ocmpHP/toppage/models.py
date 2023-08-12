8c15f6f824a9f5122bde7bc6da6069139a82f1d3
mk8877 <113814695+mk8877@users.noreply.github.com>
2023-08-12 23:34:02 +0900

    first commit

---
+++ ocmpHP/toppage/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Paper(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models .SlugField()                # URLに含まれる文字列
+    body = models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True) # 日付を自動で追加
+    
---
+++ ocmpHP/toppage/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'toppage'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
