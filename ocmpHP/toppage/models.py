7313e4df2ebc0cbac60dc99ee4ac31b2762af560
mk8877 <113814695+mk8877@users.noreply.github.com>
2023-08-31 22:00:24 +0900

    20230831

--- ocmpHP/toppage/models.py
+++ ocmpHP/toppage/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Paper(models.Model):
     title = models.CharField(max_length=100)
-    slug = models .SlugField()                # URLに含まれる文字列
+    slug = models.SlugField()                # URLに含まれる文字列
     body = models.TextField()
     posted_date = models.DateTimeField(auto_now_add=True) # 日付を自動で追加
     
